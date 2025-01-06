3ec23e6e200cd98c775365017271925ddfd22cfd
Abhay Dixit <dixitshivam534@gmail.com>
2025-01-06 23:14:08 +0530

    loops in java

---
+++ src/Loop.java
@@ -0,0 +1,11 @@
+public class Loop {
+    public static void main(String [] arg){
+        for(int i=0;i<=10000000;i++){
+
+            System.out.print(" \n");
+           
+        }
+
+       
+    }
+}
